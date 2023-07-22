62404d926874009073a9cf993e40a36368abbb0d
alexpwnz <alex7522731@yandex.ru>
2023-07-22 18:00:44 +0300

    test1

---
+++ Pages/IndexPage.cs
@@ -0,0 +1,31 @@
+﻿namespace TelegramFgisWebApp.Pages
+{
+    using System;
+    using System.Threading.Tasks;
+    using Blazor.DynamicJavascriptRuntime.Evaluator;
+    using Microsoft.AspNetCore.Components;
+    using Microsoft.JSInterop;
+
+    public class IndexPage : ComponentBase
+    {
+        public string name;
+        [Inject]
+        public IJSRuntime JsRuntime { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            
+           
+            using (dynamic context = new EvalContext(JsRuntime))
+            {
+                name = context.window.Telegram.WebApp.initDataUnsafe.user.first_name;
+            }
+
+
+            await base.OnInitializedAsync();
+        }
+
+        
+
+    }
+}
